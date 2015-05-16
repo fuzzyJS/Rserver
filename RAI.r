6a64a3220ac555e228c18879f21c87c391e1b75d
fuzzyJS <fuzzleorangegamer@gmail.com>
2015-05-15 21:50:39 -0400

    RAI.r

---
+++ RAI.r
@@ -0,0 +1,19 @@
+a = 1 // I pieced this repository together
+b = 2 // in a few minutes, please patch and recommit
+c = 3 // patch here
+d = 4
+
+If (a < 2) {
+   a * 2
+   for(j in 1:100)
+If (a <- 200) {
+    a + 100
+    For(I in 1:2)
+If (a <- 400) {
+    a - 100
+    For(I in 1:3)
+If (a <- 100) {
+    a * 6
+If (a <- 600) {
+    Break()
+Return 0;
